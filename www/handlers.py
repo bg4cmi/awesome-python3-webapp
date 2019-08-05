f0c20672304b10d5cef597ccd31d35e87f100f23
ArtyLi <bg4cmi@qq.com>
2019-08-05 18:41:09 +0800

    update

--- www/handlers.py
+++ www/handlers.py
@@ -9,9 +9,25 @@ url handlers.
 
 import re, time, json, logging, hashlib, base64, asyncio
 
+import markdown2
+
+from aiohttp import web
+
 from coroweb import get, post
+from apis import APIValueError, APIResourceNotFoundError, APIError
 
 from  models import User, Comment, Blog, next_id
+from config import configs
+
+COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
+
+def user2cookie(user, max_age):
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
 
 '''
 @get('/')
@@ -35,3 +51,45 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+'''
+def api_get_users():
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
+'''
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+@post('/api/users')
+def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_EMAIL.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll('email=?',[email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email','Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+
+    # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('uft-8')
+    return r
