0930bc4e7a4b61c24d9b64fbad367cb482c4db7e
ArtyLi <bg4cmi@qq.com>
2019-08-06 17:56:48 +0800

    updae

--- www/config_default.py
+++ www/config_default.py
@@ -13,7 +13,7 @@ configs = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'www',
-        'password': 'www',
+        'password': 'wwwwww',
         'db': 'awesome'
     },
     'session': {
