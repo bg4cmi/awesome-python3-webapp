86e48da28ef22827c4e8c27f48f2430ba1f33d39
ArtyLi <bg4cmi@qq.com>
2019-08-07 18:11:43 +0800

    update

--- www/handlers.py
+++ www/handlers.py
@@ -12,13 +12,19 @@ import markdown2
 from aiohttp import web
 
 from coroweb import get, post
-from apis import APIValueError, APIResourceNotFoundError
+#from apis import APIValueError, APIResourceNotFoundError
+from apis import Page, APIValueError, APIResourceNotFoundError, APIPermissionError, APIError
+#import apis
 
 from models import User, Comment, Blog, next_id
 from config import configs
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
 
 def user2cookie(user, max_age):
     '''
@@ -140,3 +146,21 @@ def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+@post('/api/blogs')
+def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name','name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary','summary connot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content','content cannot be empty.')
+    blog = Blog(user_id=request.__user__.id,
+    user_name=request.__user__.name,
+    user_image=request.__user__.image, 
+    name=name.strip(),
+    summary=summary.strip(),
+    content=content.strip())
+    yield from blog.save()
+    return blog
